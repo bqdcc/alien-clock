eb540a6acf8c321d4610e5aecae119e83cdf7874
Winter <bqdcc@outlook.com>
2024-01-27 13:18:54 +0800

    🔧 build:

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: "/clock/",
   server: { open: true },
   resolve: {
     alias: {
